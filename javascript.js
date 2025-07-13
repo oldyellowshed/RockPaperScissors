565c752aaeeaea31d07c6088421c7a85c7861cf7
oldyellowshed <zippy347@gmail.com>
2025-07-13 20:18:12 +0300

    Added score variables

--- javascript.js
+++ javascript.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
     let choiceComputer = Math.floor(Math.random() * 3);
     
     if(choiceComputer === 0) {
-        console.log("Rock")
+        console.log("Rock");
     } else if (choiceComputer === 1) {
-        console.log("Paper")
+        console.log("Paper");
     } else if (choiceComputer === 2) {
-        console.log("Scissors")
+        console.log("Scissors");
     }
 }
 
@@ -14,11 +14,13 @@ function getHumanChoice() {
     let choiceHuman = prompt("Rock? Paper? Or Scissors?")
 
     if(choiceHuman === "Rock") {
-        console.log("Rock")
+        console.log("Rock");
     } else if (choiceHuman === "Paper") {
-        console.log("Paper")
+        console.log("Paper");
     } else if (choiceHuman === "Scissors") {
-        console.log("Scissors")
+        console.log("Scissors");
     }
 }
 
+let humanScore = 0;
+let computerScore = 0;
