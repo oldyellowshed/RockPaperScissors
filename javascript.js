67146ea5d4d25725f4294ca7f2196bf31b190a66
oldyellowshed <zippy347@gmail.com>
2025-07-13 19:54:04 +0300

    Added function for computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let choiceComputer = Math.floor(Math.random() * 3);
+    
+    if(choiceComputer === 0) {
+        console.log("Rock")
+    } else if (choiceComputer === 1) {
+        console.log("Paper")
+    } else if (choiceComputer === 2) {
+        console.log("Scissors")
+    }
+
+    alert(choiceComputer)
+}
+
+getComputerChoice()
