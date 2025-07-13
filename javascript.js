bbe59534715a349e5b6d8c3dba39e94eb8e54b34
oldyellowshed <zippy347@gmail.com>
2025-07-13 23:07:08 +0300

    Added function for a single round

--- javascript.js
+++ javascript.js
@@ -24,3 +24,26 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    if(humanChoice === "Rock" && computerChoice === "Rock"); {
+        console.log("DRAW!");
+    } else if (humanChoice === "Rock" && computerChoice === "Paper"); {
+        console.log("Computer wins!");
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors"); {
+        console.log("You win!");
+    } else if (humanChoice === "Paper" && computerChoice === "Rock"); {
+        console.log("You win!!");
+    } else if (humanChoice === "Paper" && computerChoice === "Paper"); {
+        console.log("DRAW!");
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors"); {
+        console.log("Computer wins!");
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock"); {
+        console.log("Computer wins!!");
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper"); {
+        console.log("You win!"); 
+    } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
+        console.log("DRAW!");
+    }
+} 
