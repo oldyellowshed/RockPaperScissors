69733eb9ac03c37f078d3bb5d496d5957515277b
oldyellowshed <zippy347@gmail.com>
2025-07-15 23:37:00 +0300

    Added play a single round function

--- javascript.js
+++ javascript.js
@@ -27,23 +27,28 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
 
-    if(humanChoice === "Rock" && computerChoice === "Rock"); {
+    if(humanChoice === "Rock" && computerChoice === "Rock") {
         console.log("DRAW!");
-    } else if (humanChoice === "Rock" && computerChoice === "Paper"); {
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
         console.log("Computer wins!");
-    } else if (humanChoice === "Rock" && computerChoice === "Scissors"); {
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
         console.log("You win!");
-    } else if (humanChoice === "Paper" && computerChoice === "Rock"); {
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
         console.log("You win!!");
-    } else if (humanChoice === "Paper" && computerChoice === "Paper"); {
+    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
         console.log("DRAW!");
-    } else if (humanChoice === "Paper" && computerChoice === "Scissors"); {
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
         console.log("Computer wins!");
-    } else if (humanChoice === "Scissors" && computerChoice === "Rock"); {
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
         console.log("Computer wins!!");
-    } else if (humanChoice === "Scissors" && computerChoice === "Paper"); {
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
         console.log("You win!"); 
     } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
         console.log("DRAW!");
     }
 } 
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection)
